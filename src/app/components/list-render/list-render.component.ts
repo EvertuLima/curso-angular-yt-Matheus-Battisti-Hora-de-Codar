f90ec625cef04ab8e6e7ce109d08a2b178da0438
EvertuLima <78697248+EvertuLima@users.noreply.github.com>
2023-09-24 21:30:11 -0300

    interface

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Animal } from 'src/app/Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -7,16 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ListRenderComponent {
 
-  animals = [
+  animals: Animal[] = [
     { name: 'Turca', type: 'Dog', age: 12 },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Frida', type: 'Dog'},
-    { name: 'Bob', type: 'Horse' },
+    { name: 'Tom', type: 'Cat', age: 3 },
+    { name: 'Frida', type: 'Dog', age: 16},
+    { name: 'Bob', type: 'Horse', age: 13},
   ];
 
-  animal = {
+  animal: Animal = {
     name: 'Teste',
-    type: 'Algo'
+    type: 'Algo',
+    age: 1
+  }
+
+  animalDetails = ''
+
+  showAge(animal: Animal){
+    // window.alert(animal.age)
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`
   }
 
 }
