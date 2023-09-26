53922c0ca0f73a29fd11a1632f56f187bf548d09
EvertuLima <78697248+EvertuLima@users.noreply.github.com>
2023-09-26 12:49:48 -0300

    roteamento dos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,25 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { PipesComponent } from './components/pipes/pipes.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 const routes: Routes = [
   {path: '', component: FirstComponentComponent},
-  {path: 'list', component: ListRenderComponent}
+  {path: 'parent-data', component: ParentDataComponent},
+  {path: 'directives', component: DirectivesComponent},
+  {path: 'if-render', component: IfRenderComponent},
+  {path: 'eventos', component: EventosComponent},
+  {path: 'emitter', component: EmitterComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'pipes', component: PipesComponent},
+  {path: 'two-way', component: TwoWayBindingComponent},
+
 ];
 
 @NgModule({
